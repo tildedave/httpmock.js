8c43a150636d9897cd55d17d44013c5c9086dfd9
Dave King <tildedave@gmail.com>
2011-09-21 05:22:54 -0400

    Only add DSL namespacing inside of a local function.

--- src/mock.js
+++ src/mock.js
@@ -1,25 +1,6 @@
 var fs = require('fs');
 var http = require('http');
 
-/**
- * DSL Namespacing
- */
-
-var uri = require('./request.js').uri;
-var request = require('./request.js').request;
-var method = require('./request.js').method;
-
-var response = require('./response.js').response;
-var text = require('./response.js').text;
-var json = require('./response.js').json;
-var status = require('./response.js').status;
-var time = require('./response.js').time;
-var otherwise = require('./response.js').otherwise;
-
-
-var route = require('./route.js').route;
-var routes = require('./route.js').routes;
-
 var mock = function (port, file) {
   fs.readFile(file, encoding="ascii", function (err, data) {
     console.log("Starting server with configuration:");
@@ -27,12 +8,35 @@ var mock = function (port, file) {
     console.log(data);
     console.log("------------------------------------------------");
     
-    var r = eval(data);
+    var __routes;
+      (function () {
+
+        /**
+         * DSL Namespacing
+         */
+
+        var uri = require('./request.js').uri;
+        var request = require('./request.js').request;
+        var method = require('./request.js').method;
+        
+        var response = require('./response.js').response;
+        var text = require('./response.js').text;
+        var json = require('./response.js').json;
+        var status = require('./response.js').status;
+        var time = require('./response.js').time;
+        var otherwise = require('./response.js').otherwise;
+        
+        var route = require('./route.js').route;
+        var routes = require('./route.js').routes;
+
+        __routes = eval(data);
+      }());
+    
     console.log("Starting server on port " + port);
     
     http.createServer(function (req, res) {
       console.log(new Date() + " -- " + req.method + " " + req.url);
-      r.process(req, res);
+      __routes.process(req, res);
     }).listen(port, "0.0.0.0");
   });
 };
